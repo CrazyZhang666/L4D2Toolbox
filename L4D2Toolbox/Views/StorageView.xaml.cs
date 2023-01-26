759e9e8e6357f284f982cffe95bffcc36607fd8e
穗乃果 <1763575579@qq.com>
2023-01-26 11:46:05 +0800

    update func

--- L4D2Toolbox/Views/StorageView.xaml.cs
+++ L4D2Toolbox/Views/StorageView.xaml.cs
@@ -1,28 +1,129 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using L4D2Toolbox.Data;
+using L4D2Toolbox.Utils;
 
-namespace L4D2Toolbox.Views
+using Steamworks;
+
+namespace L4D2Toolbox.Views;
+
+/// <summary>
+/// StorageView.xaml 的交互逻辑
+/// </summary>
+public partial class StorageView : UserControl
 {
     /// <summary>
-    /// StorageView.xaml 的交互逻辑
+    /// 玩家Steam云存储文件列表动态集合
     /// </summary>
-    public partial class StorageView : UserControl
+    public ObservableCollection<StorageInfo> StorageInfoLists { get; set; } = new();
+
+    public StorageView()
     {
-        public StorageView()
+        InitializeComponent();
+        this.DataContext = this;
+    }
+
+    /// <summary>
+    /// 超链接请求导航事件
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        ProcessUtil.OpenLink(e.Uri.OriginalString);
+        e.Handled = true;
+    }
+
+    /// <summary>
+    /// 刷新列表
+    /// </summary>
+    private void RefreshList()
+    {
+        Button_RefreshStorageList.IsEnabled = false;
+
+        int index = 1;
+        StorageInfoLists.Clear();
+
+        foreach (var file in SteamRemoteStorage.Files)
         {
-            InitializeComponent();
+            this.Dispatcher.Invoke(DispatcherPriority.Background, () =>
+            {
+                StorageInfoLists.Add(new()
+                {
+                    Index = index++,
+                    Name = file,
+                    Size = MiscUtil.ByteConverterMB(SteamRemoteStorage.FileSize(file)),
+                    Date = MiscUtil.FormatDateTime(SteamRemoteStorage.FileTime(file)),
+                    Exists = MiscUtil.BoolToFlag(SteamRemoteStorage.FileExists(file)),
+                    Persisted = MiscUtil.BoolToFlag(SteamRemoteStorage.FilePersisted(file))
+                });
+            });
+        }
+
+        RefushQuotaInfo();
+
+        Button_RefreshStorageList.IsEnabled = true;
+    }
+
+    /// <summary>
+    /// 刷新配额信息
+    /// </summary>
+    private void RefushQuotaInfo()
+    {
+        var quotaBytes = SteamRemoteStorage.QuotaBytes;
+        var quotaUsedBytes = SteamRemoteStorage.QuotaUsedBytes;
+
+        Border_QuotaUse.Width = 1.0 * quotaUsedBytes / quotaBytes * 300;
+        TextBlock_QuotaInfo.Text = $"{MiscUtil.ByteConverterMB(quotaUsedBytes)} 已存储 / {MiscUtil.ByteConverterMB(quotaBytes)} 总大小";
+    }
+
+    /// <summary>
+    /// 刷新列表
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Button_RefreshStorageList_Click(object sender, RoutedEventArgs e)
+    {
+        RefreshList();
+    }
+
+    /// <summary>
+    /// 删除选中项
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Button_DeleteStorage_Click(object sender, RoutedEventArgs e)
+    {
+        if (ListBox_Storages.SelectedItem is StorageInfo info)
+        {
+            if (MessageBox.Show($"您确定要删除这个文件吗？此操作不可撤销！\n\n文件名：{info.Name}\n文件大小：{info.Size}",
+                "删除文件", MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.OK)
+            {
+                if (SteamRemoteStorage.FileDelete(info.Name))
+                    MsgBoxUtil.Information($"删除文件 {info.Name} 成功");
+                else
+                    MsgBoxUtil.Error($"删除文件 {info.Name} 失败");
+
+                RefreshList();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 清空全部文件
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Button_ClearStorage_Click(object sender, RoutedEventArgs e)
+    {
+        if (MessageBox.Show($"您确定要清空Steam云存储文件吗？此操作不可撤销！",
+            "清空Steam云存储文件", MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.OK)
+        {
+            foreach (var file in SteamRemoteStorage.Files)
+            {
+                SteamRemoteStorage.FileDelete(file);
+            }
+
+            MsgBoxUtil.Information("清空Steam云存储文件成功");
+            RefreshList();
         }
     }
 }
