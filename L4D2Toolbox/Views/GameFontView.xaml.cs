7da17eda4dca34b6eb34f04673e80e295dd22fb9
穗乃果 <1763575579@qq.com>
2023-01-27 13:48:12 +0800

    update font func

--- L4D2Toolbox/Views/GameFontView.xaml.cs
+++ L4D2Toolbox/Views/GameFontView.xaml.cs
@@ -17,21 +17,24 @@ public partial class GameFontView : UserControl
         this.DataContext = this;
         MainWindow.WindowClosingEvent += MainWindow_WindowClosingEvent;
 
-        TextBox_SampleText.AppendText("让大家翘首以盼的的《求生之路 2》（L4D2）以僵尸大灾难为背景，是 2008 年最受欢迎且屡获殊荣的合作游戏《求生之路》的续集。\n");
-        TextBox_SampleText.AppendText("这个第一人称射击恐怖合作动作游戏将带领玩家和好友穿过美国南部的城市、沼泽和墓地，从萨凡纳到新奥尔良，沿途经过五个漫长的战役。\n");
-        TextBox_SampleText.AppendText("玩家将扮演四名新生还者中的一名，装备有种类繁多、数量惊人的经典及先进武器。 除了枪支之外，玩家还有机会用各种可制造屠杀的近战武器在感染者上泄愤，例如电锯、斧头、甚至是致命的平底锅。\n");
+        TextBox_SampleText.AppendText("让大家翘首以盼的的《求生之路 2》（L4D2）以僵尸大灾难为背景，是 2008 年最受欢迎且屡获殊荣的合作游戏《求生之路》的续集。\n\n");
+        TextBox_SampleText.AppendText("这个第一人称射击恐怖合作动作游戏将带领玩家和好友穿过美国南部的城市、沼泽和墓地，从萨凡纳到新奥尔良，沿途经过五个漫长的战役。\n\n");
+        TextBox_SampleText.AppendText("玩家将扮演四名新生还者中的一名，装备有种类繁多、数量惊人的经典及先进武器。 除了枪支之外，玩家还有机会用各种可制造屠杀的近战武器在感染者上泄愤，例如电锯、斧头、甚至是致命的平底锅。");
 
         // 读取对应配置文件
-        TextBox_CustomFontName.Text = IniHelper.ReadValue("Font", "CustomFontName");
-        TextBox_CustomRunArgs.Text = IniHelper.ReadValue("Font", "CustomRunArgs");
+        var CustomFontName = IniHelper.ReadValue("Font", "CustomFontName");
+        var CustomRunArgs = IniHelper.ReadValue("Font", "CustomRunArgs");
 
-        if (string.IsNullOrWhiteSpace(TextBox_CustomRunArgs.Text.Trim()))
+        TextBox_CustomFontName.Text = CustomFontName;
+        TextBox_CustomRunArgs.Text = CustomRunArgs;
+
+        if (string.IsNullOrWhiteSpace(CustomRunArgs))
             TextBox_CustomRunArgs.Text = "-steam -novid -language schinese";
 
         Task.Run(() =>
         {
-            var fonts = new List<string>();
             // 获取系统所有字体
+            var fonts = new List<string>();
             foreach (var family in Fonts.SystemFontFamilies)
             {
                 foreach (var item in family.FamilyNames)
@@ -48,6 +51,15 @@ public partial class GameFontView : UserControl
                     ListBox_Fonts.Items.Add(font);
                 });
             });
+
+            var index = fonts.IndexOf(CustomFontName);
+            if (index != -1)
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    ListBox_Fonts.SelectedIndex = index;
+                });
+            }
         });
     }
 
