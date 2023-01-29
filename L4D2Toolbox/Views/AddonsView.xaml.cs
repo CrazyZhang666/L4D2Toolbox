e4d6a446629af55e796e5d004028cf72646a2c61
穗乃果 <1763575579@qq.com>
2023-01-29 14:11:05 +0800

    fix bug

--- L4D2Toolbox/Views/AddonsView.xaml.cs
+++ L4D2Toolbox/Views/AddonsView.xaml.cs
@@ -129,31 +129,25 @@ public partial class AddonsView : UserControl
         {
             package.ReadEntry(addoninfo, out byte[] output);
 
-            using var memoryStream = new MemoryStream(output);
+            var str = Encoding.UTF8.GetString(output);
+            if (str.Contains('�'))
+                str = Encoding.GetEncoding("GB2312").GetString(output);
 
-            var streamReader = new StreamReader(memoryStream, Encoding.UTF8);
-            if (streamReader.ReadToEnd().Contains('�'))
-                streamReader = new StreamReader(memoryStream, Encoding.GetEncoding("GB2312"));
+            var lines = str.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
 
-            streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
-            streamReader.DiscardBufferedData();
-
-            string line = string.Empty;
-            while ((line = streamReader.ReadLine()) != null)
+            foreach (var line in lines)
             {
-                if (line.Trim().ToLower().StartsWith("addontitle"))
+                var title = line.Replace("\"", "").Trim().ToLower();
+                if (title.StartsWith("addontitle"))
                 {
-                    streamReader.Close();
-                    memoryStream.Close();
-
-                    line = line.Replace("addontitle", "", StringComparison.OrdinalIgnoreCase).Trim();
-                    var index = line.LastIndexOf("\"");
+                    title = line.Replace("addontitle", "", StringComparison.OrdinalIgnoreCase).Trim();
+                    var index = title.LastIndexOf("\"");
                     if (index != -1)
                     {
-                        line = line[..index].Replace("\"", "").Trim();
+                        title = title[..index].Replace("\"", "").Trim();
                     }
 
-                    return string.IsNullOrEmpty(line) ? "<无标题>" : line;
+                    return string.IsNullOrEmpty(title) ? "<无标题>" : title;
                 }
             }
         }
